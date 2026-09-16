3025c2f462646f3f98987dd3fcab305ab339256b
Godwin024 <simondavid5220@gmail.com>
2026-09-16 22:36:27 +0100

    in progress

--- src/hugeInteger/HugeInteger.java
+++ src/hugeInteger/HugeInteger.java
@@ -1,4 +1,48 @@
 package hugeInteger;
 
 public class HugeInteger {
+    private int[] digit = new int [40];
+    private int size;
+
+    public void parse(String input) {
+        size = input.length();
+        for(int count = 0; count < size; count++){
+            digit[count] = input.charAt(count) - '0';
+        }
+    }
+
+    @Override
+    public String toString(){
+        StringBuilder result = new StringBuilder();
+        for(int count = 0; count < size; count++){
+            result.append(digit[count]);
+        }
+        return result.toString();
+    }
+
+
+    public boolean isZero() {
+        for(int count = 0; count < size; count++){
+            if(digit[count] != 0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean isEqualTo(HugeInteger other) {
+        if(size != other.size){
+            return false;
+        }
+        for(int count = 0; count < size; count++){
+            if(digit[count] != other.digit[count]){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public HugeInteger add(HugeInteger secondChar) {
+        return null;
+    }
 }
--- test/hugeInteger/HugeIntegerTest.java
+++ test/hugeInteger/HugeIntegerTest.java
@@ -1,4 +1,42 @@
 package hugeInteger;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class HugeIntegerTest {
+    @Test
+    public void testParseAndtoStringReturnsTheSameNumber() {
+        HugeInteger hugeInteger = new HugeInteger();
+        hugeInteger.parse("123456");
+        assertEquals("123456", hugeInteger.toString());
+    }
+
+    @Test
+    public void testisZeroShouldReturnTrueForZero(){
+    HugeInteger hugeInteger = new HugeInteger();
+    hugeInteger.parse("000");
+    assertTrue(hugeInteger.isZero());
+
+    }
+
+    @Test
+    public void testTwoHugeIntegerWithTheSamevalueShoudBeEqual(){
+        HugeInteger firstChar = new HugeInteger();
+        HugeInteger secondChar = new HugeInteger();
+        firstChar.parse("1235");
+        secondChar.parse("1235");
+        assertTrue(firstChar.isEqualTo(secondChar));
+
+    }
+    @Test
+    public void testAddition(){
+        HugeInteger firstChar = new HugeInteger();
+        HugeInteger secondChar = new HugeInteger();
+        firstChar.parse("999");
+        secondChar.parse("1");
+        HugeInteger sum = firstChar.add(secondChar);
+        assertEquals("1000", sum.toString());
+    }
 }
